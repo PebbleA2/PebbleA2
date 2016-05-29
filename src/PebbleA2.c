6a2d8726c69f74b4005746d6e8f92db9ebe8ea00
ishotjr <github@ishotjr.com>
2016-05-29 19:29:01 -0400

    :name_badge: :bookmark_tabs: Rename static variables

--- src/PebbleA2.c
+++ src/PebbleA2.c
@@ -1,41 +1,42 @@
 #include <pebble.h>
 
-static Window *window;
-static TextLayer *text_layer;
+static Window *s_window;
+static TextLayer *s_text_layer;
 
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
-  text_layer_set_text(text_layer, "Test");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  s_text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
+  text_layer_set_text(s_text_layer, "Test");
+  text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+  text_layer_destroy(s_text_layer);
 }
 
 static void init(void) {
-  window = window_create();
-  window_set_window_handlers(window, (WindowHandlers) {
+  s_window = window_create();
+  window_set_window_handlers(s_window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
   });
   const bool animated = true;
-  window_stack_push(window, animated);
+  window_stack_push(s_window, animated);
 }
 
 static void deinit(void) {
-  window_destroy(window);
+  window_destroy(s_window);
 }
 
 int main(void) {
   init();
 
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", s_window);
 
   app_event_loop();
   deinit();
 }
+  
