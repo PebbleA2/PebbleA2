b074f87904d8ce9c2d2813c6cbabc49b4a0bbb43
ishotjr <github@ishotjr.com>
2016-05-29 22:01:42 -0400

    :straight_ruler: :watch: Mock up hh:mm

--- src/PebbleA2.c
+++ src/PebbleA2.c
@@ -1,20 +1,39 @@
 #include <pebble.h>
 
 static Window *s_window;
-static TextLayer *s_text_layer;
+static TextLayer *s_time_layer;
 
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  s_text_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
-  text_layer_set_text(s_text_layer, "Test");
-  text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
+  // calculate centered rect based on text/font
+  GPoint left_center = GPoint(0, bounds.size.h / 2);
+  GRect bounding_box = GRect(left_center.x, left_center.y, bounds.size.w, 42 + 16);
+
+  GFont time_font = fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD);
+  GTextOverflowMode time_overflow = GTextOverflowModeTrailingEllipsis;
+  GTextAlignment time_alignment = GTextAlignmentCenter;
+
+  GSize optimal_size = graphics_text_layout_get_content_size("00:00", time_font, bounding_box, time_overflow, time_alignment);
+  GPoint optimal_point = GPoint(left_center.x + ((bounds.size.w - optimal_size.w) / 2), left_center.y - (optimal_size.h / 2));
+  optimal_point.y -= 6; // fudge factor due to weird font rendering
+
+  s_time_layer = text_layer_create(GRect(optimal_point.x, optimal_point.y, optimal_size.w, optimal_size.h));
+  text_layer_set_text(s_time_layer, "00:00");
+  text_layer_set_font(s_time_layer, time_font);
+  
+  text_layer_set_overflow_mode(s_time_layer, time_overflow);
+  text_layer_set_text_alignment(s_time_layer, time_alignment);
+
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(s_text_layer);
+  text_layer_destroy(s_time_layer);
 }
 
 static void init(void) {
