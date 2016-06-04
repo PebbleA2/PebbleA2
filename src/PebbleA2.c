c3c94c9ab28336f1d11984a53a79dbe09d098837
ishotjr <github@ishotjr.com>
2016-06-04 16:44:36 -0400

    :hourglass_flowing_sand: :watch: Add tick timer event service handler

--- src/PebbleA2.c
+++ src/PebbleA2.c
@@ -3,6 +3,24 @@
 static Window *s_window;
 static TextLayer *s_time_layer;
 
+static void update_time() {
+
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+
+  // Create a long-lived buffer containing hh:mm
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, s_buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
@@ -20,7 +38,7 @@ static void window_load(Window *window) {
   optimal_point.y -= 6; // fudge factor due to weird font rendering
 
   s_time_layer = text_layer_create(GRect(optimal_point.x, optimal_point.y, optimal_size.w, optimal_size.h));
-  text_layer_set_text(s_time_layer, "00:00");
+  //text_layer_set_text(s_time_layer, "00:00");
   text_layer_set_font(s_time_layer, time_font);
   
   text_layer_set_overflow_mode(s_time_layer, time_overflow);
@@ -44,6 +62,12 @@ static void init(void) {
   });
   const bool animated = true;
   window_stack_push(s_window, animated);
+
+  // Make sure the time is displayed from the start
+  update_time();
+
+  // Register with TickTimerService (MINUTE_UNIT in order to save on battery usage)
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
 static void deinit(void) {
