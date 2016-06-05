5cf0730cda233fac17d20ff5447f3f033948fb13
ishotjr <github@ishotjr.com>
2016-06-05 01:49:25 -0400

    :calling: :octocat: Add call to gh-pages API, twice hourly

--- src/PebbleA2.c
+++ src/PebbleA2.c
@@ -47,6 +47,19 @@ static void update_time() {
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
+
+  // update message every 30 minutes
+  if(tick_time->tm_min % 30 == 0) {
+    // Begin dictionary
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+
+    // Add a key-value pair
+    dict_write_uint8(iter, 0, 0);
+
+    // Send the message!
+    app_message_outbox_send();
+  }
 }
 
 static void window_load(Window *window) {
--- src/js/app.js
+++ src/js/app.js
@@ -1,44 +1,62 @@
-function getMessage() {
-	responseText = '{"message": "PebbleA2 rocks!!! :D"}';
+//"use strict";
 
-	// responseText is hard-coded for now
-	var json = JSON.parse(responseText);
-	console.log(responseText); // JSON.stringify() not necessary!
-
-	// TODO: error checking???
-	var message = json.message;
-	console.log('message is "' + message + '"');
-
-	// Assemble dictionary using our keys
-	var dictionary = {
-		"message": message
+// XMLHttpRequest helper
+var xhrRequest = function (url, type, callback) {
+	var xhr = new XMLHttpRequest();
+	xhr.onload = function () {
+		callback(this.responseText);
 	};
+	xhr.open(type, url);
+	xhr.send();
+};
 
-	// Send to Pebble
-	Pebble.sendAppMessage(dictionary,
-		function(e) {
-			console.log("AppMessage sent to Pebble successfully!");
-		},
-		function(e) {
-			console.log("Error sending AppMessage to Pebble!");
-		}
+function getMessage() {
+	//responseText = '{"message": "PebbleA2 rocks!!! :D"}';
+
+	var url = 'http://pebblea2.github.io/PebbleA2/message.json';
+
+	xhrRequest(url, 'GET', 
+		function(responseText) {
+			var json = JSON.parse(responseText);
+			console.log(responseText); // JSON.stringify() not necessary!
+
+			// TODO: error checking???
+			var message = json.message;
+			console.log('message is "' + message + '"');
+
+			// Assemble dictionary using our keys
+			var dictionary = {
+				"message": message
+			};
+
+			// Send to Pebble
+			Pebble.sendAppMessage(dictionary,
+				function(e) {
+					console.log("AppMessage sent to Pebble successfully!");
+				},
+				function(e) {
+					console.log("Error sending AppMessage to Pebble!");
+				}
+			);
+		}      
 	);
+
 }
 
 // Listen for when the watchface is opened
 Pebble.addEventListener('ready', 
-  function(e) {
-    console.log('PebbleKit JS ready!');
+	function(e) {
+		console.log('PebbleKit JS ready!');
 
-    // Get the message
-    getMessage();
-  }
+		// Get the message
+		getMessage();
+	}
 );
 
 // Listen for when an AppMessage is received
 Pebble.addEventListener('appmessage',
-  function(e) {
-    console.log('AppMessage received!');
-    getMessage();
-  }                     
+	function(e) {
+		console.log('AppMessage received!');
+		getMessage();
+	}                     
 );
